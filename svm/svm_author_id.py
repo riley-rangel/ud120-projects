8cf7a4305863587be69d10209b9d9cc1bca3f584
Riley Rangel <rrangel91@gmail.com>
2020-07-16 14:40:09 -0700

    train and test the svm classifier

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,6 +26,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+score = accuracy_score(pred, labels_test)
+print(score)
 
 #########################################################
 
